a8e0440d822e1f8d114e32827a11cc23be544540
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2024-01-21 10:39:12 -0500

    clean Up

--- README.md
+++
@@ -1,2 +0,0 @@
-# This is Google Clone Made by Varun Sharma.
-Under the guidance of Sir Deepak Viswakarma
--- src/keys.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "AIzaSyBRb5A-q9ZwEXcL8AAvrcpJVs1WEgSe0wc";
-
-export default API_KEY;
